5fea8768a5e0eed4d056521d040392ead65f9dbb
deverser <alexmal74@gmail.com>
2019-02-24 22:44:12 +0500

    массив $goods с товарами переделан в многомерный массив для улучшения читабельности кода

--- index.php
+++ index.php
@@ -8,31 +8,12 @@ $user_avatar = 'img/user.jpg';
 
 $categories = array('Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное');
 $goods = array(
-        'id_1'=>array('name'=>'2014 Rossignol District Snowboard',
-                      'category'=>'Доски и лыжи',
-                      'price'=> 10990,
-                      'img'=>'img/lot-1.jpg'),
-         'id_2'=>array('name'=>'DC Ply Mens 2016/2017 Snowboard',
-                       'category'=>'Доски и лыжи',
-                       'price'=> 159999,
-                       'img'=>'img/lot-2.jpg'),
-          'id_3'=>array('name'=>'Крепления Union Contact Pro 2015 года размер L/XL',
-                        'category'=>'Крепления',
-                        'price'=> 8000,
-                        'img'=>'img/lot-3.jpg'),
-          'id_4'=>array('name'=>'Ботинки для сноуборда DC Mutiny Charcoal',
-                        'category'=>'Ботинки',
-                        'price'=> 10999,
-                        'img'=>'img/lot-4.jpg'),
-          'id_5'=>array('name'=>'Куртка для сноуборда DC Mutiny Charcoal',
-                        'category'=>'Одежда',
-                        'price'=> 7500,
-                        'img'=>'img/lot-5.jpg'),
-          'id_6'=>array('name'=>'Маска Oakley Canopy',
-                        'category'=>'Разное',
-                        'price'=> 5400,
-                        'img'=>'img/lot-6.jpg')
-                );
+          ['name'=>'2014 Rossignol District Snowboard', 'category'=>'Доски и лыжи', 'price'=> 10990, 'img'=>'img/lot-1.jpg'],
+          ['name'=>'DC Ply Mens 2016/2017 Snowboard', 'category'=>'Доски и лыжи', 'price'=> 159999, 'img'=>'img/lot-2.jpg'],
+          ['name'=>'Крепления Union Contact Pro 2015 года размер L/XL', 'category'=>'Крепления', 'price'=> 8000, 'img'=>'img/lot-3.jpg'],
+          ['name'=>'Ботинки для сноуборда DC Mutiny Charcoal', 'category'=>'Ботинки', 'price'=> 10999, 'img'=>'img/lot-4.jpg'],
+          ['name'=>'Куртка для сноуборда DC Mutiny Charcoal', 'category'=>'Одежда', 'price'=> 7500, 'img'=>'img/lot-5.jpg'],
+          ['name'=>'Маска Oakley Canopy', 'category'=>'Разное', 'price'=> 5400, 'img'=>'img/lot-6.jpg']);
 
 $page_content = renderTemplate('templates/index.php', ['goods' => $goods]);
 $layout = renderTemplate('templates/layout.php',
