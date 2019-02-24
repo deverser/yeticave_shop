9c701e8b8096deb2cc2ac93ebc7f8e812fa9e828
deverser <alexmal74@gmail.com>
2019-02-24 22:27:48 +0500

    Добавлен шаблон index.php для генерации динамического контента на главной странице магазина

---
+++ templates/index.php
@@ -0,0 +1,59 @@
+<?php include_once '../functions.php' ?>
+<section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+
+            <!-- Отоброжение категорий товаров на сайте -->
+
+            <li class="promo__item promo__item--attachment">
+                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
+            </li>
+            <li class="promo__item promo__item--attachment">
+                <a class="promo__link" href="all-lots.html">Крепления</a>
+            </li>
+            <li class="promo__item promo__item--boots">
+                <a class="promo__link" href="all-lots.html">Ботинки</a>
+            </li>
+            <li class="promo__item promo__item--clothing">
+                <a class="promo__link" href="all-lots.html">Одежда</a>
+            </li>
+            <li class="promo__item promo__item--tools">
+                <a class="promo__link" href="all-lots.html">Инструменты</a>
+            </li>
+            <li class="promo__item promo__item--other">
+                <a class="promo__link" href="all-lots.html">Разное</a>
+            </li>
+
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+
+            <!-- Вывод карточек с товарами -->
+
+            <?php foreach ($goods as $item): ?>
+                    <li class="lots__item lot">
+                        <div class="lot__image">
+                            <img src="<?= $item['img']; ?>" width="350" height="260" alt="Сноуборд">
+                        </div>
+                        <div class="lot__info">
+                            <span class="lot__category"><?= $item['category']; ?></span>
+                            <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $item['name']; ?></a></h3>
+                            <div class="lot__state">
+                                <div class="lot__rate">
+                                    <span class="lot__amount">Стартовая цена</span>
+                                    <span class="lot__cost"><?= priceFormat($item['price']); ?><!--<b class="rub">р</b>--></span>
+                                </div>
+                                <div class="lot__timer timer">
+                                    16:45:32
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+        </ul>
+    </section>
